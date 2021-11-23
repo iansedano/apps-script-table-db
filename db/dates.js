6bb0cfae29dc66d3847c08d71ad1e1c75a641ed5
Ian <iansedano@gmail.com>
2021-11-23 09:28:51 +0100

    adding todo

--- db/dates.js
+++ db/dates.js
@@ -1,114 +1,139 @@
 function setDateToMidnight(inputDate, nextDay) {
-    const [year, month, date] = [
-        inputDate.getFullYear(),
-        inputDate.getMonth(),
-        inputDate.getDate()
-    ]
+  const [year, month, date] = [
+    inputDate.getFullYear(),
+    inputDate.getMonth(),
+    inputDate.getDate()
+  ];
 
-    if (nextDay === false) {
-        return new Date(year, month, date)
-    } else if (nextDay === true) {
-        return addDays(output, 1)
-    }
-
+  if (nextDay === false) {
+    return new Date(year, month, date);
+  } else if (nextDay === true) {
+    return addDays(output, 1);
+  }
 }
 
 function YMDMatch(dateA, dateB) {
-    const [yearA, monthA, dayOfMonthA] = [
-        dateA.getFullYear(),
-        dateA.getMonth(),
-        dateA.getDate()
-    ]
+  const [yearA, monthA, dayOfMonthA] = [
+    dateA.getFullYear(),
+    dateA.getMonth(),
+    dateA.getDate()
+  ];
 
-    const [yearB, monthB, dayOfMonthB] = [
-        dateB.getFullYear(),
-        dateB.getMonth(),
-        dateB.getDate()
-    ]
+  const [yearB, monthB, dayOfMonthB] = [
+    dateB.getFullYear(),
+    dateB.getMonth(),
+    dateB.getDate()
+  ];
 
-    if (
-        yearA === yearB &&
-        monthA === monthB &&
-        dayOfMonthA === dayOfMonthB
-    ) return true
+  if (yearA === yearB && monthA === monthB && dayOfMonthA === dayOfMonthB)
+    return true;
 
-    return false
+  return false;
 }
 
 function getYMDComponentsFromDate(date) {
-    const isDateObject = 
-        date
-        &&
-        Object.prototype.toString.call(date) === '[object Date]'
-        &&
-        !isNaN(date)
-    
-    if (isDateObject) {
-        const year = date.getFullYear()
-        let month = date.getMonth() + 1
-        let day = date.getDate()
+  const isDateObject =
+    date &&
+    Object.prototype.toString.call(date) === "[object Date]" &&
+    !isNaN(date);
 
-        return [year, month, day]
+  if (isDateObject) {
+    const year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
 
-    } else throw "not a date!"
+    return [year, month, day];
+  } else throw "not a date!";
 }
-
 
 /**
  * This function takes in a date and returns two dates spanning
  * Monday to Sunday (7 days).
  */
- function getWeekBoundaries(givenDate) {
+function getWeekBoundaries(givenDate) {
+  const BOUNDARY_MAP = {
+    1: [0, 6],
+    2: [-1, 5],
+    3: [-2, 4],
+    4: [-3, 3],
+    5: [-4, 2],
+    6: [-5, 1],
+    0: [-6, 0]
+  };
 
-    const BOUNDARY_MAP = {
-        1: [ 0, 6],
-        2: [-1, 5],
-        3: [-2, 4],
-        4: [-3, 3],
-        5: [-4, 2],
-        6: [-5, 1],
-        0: [-6, 0]
-      }
+  const startingDay = givenDate.getDay();
 
-    const startingDay = givenDate.getDay()
-  
-    const startDate = addDays(givenDate, BOUNDARY_MAP[startingDay][0])
-    const endDate = addDays(givenDate, BOUNDARY_MAP[startingDay][1])
-  
-    // if (endDate.getTime() - startDate.getTime() !== 691200000) {
-    //   throw "Something went wrong while calculating week"
-    // }
-    return [startDate, endDate]
+  const startDate = addDays(givenDate, BOUNDARY_MAP[startingDay][0]);
+  const endDate = addDays(givenDate, BOUNDARY_MAP[startingDay][1]);
+
+  // if (endDate.getTime() - startDate.getTime() !== 691200000) {
+  //   throw "Something went wrong while calculating week"
+  // }
+  return [startDate, endDate];
+}
+
+function getWeekList(date) {
+  const [start, end] = getWeekBoundaries(date);
+
+  const week = [start];
+
+  for (let i = 0; i != 5; i++) {
+    week.push(addDays(week[i], 1));
   }
+  week.push(end);
+  return week;
+}
 
-  function getWeekList(date) {
-    const [start, end] = getWeekBoundaries(date);
+function addDays(date, days) {
+  const output = new Date(date);
+  output.setDate(output.getDate() + days);
+  return output;
+}
 
-    const week = [start]
+function getMidnight(inputDate) {
+  const [year, month, date] = [
+    inputDate.getFullYear(),
+    inputDate.getMonth(),
+    inputDate.getDate()
+  ];
+  return new Date(year, month, date);
+}
 
-    for (let i=0; i!=5; i++) {
-        week.push(addDays(week[i], 1))
-    }
-    week.push(end)
-    return week
-  }
-  
-  function addDays(date, days) {
-    const output = new Date(date);
-    output.setDate(output.getDate() + days);
+function msToHrs(t) {
+  return (t / 1000 / 60 / 60).toPrecision(2);
+}
+
+function setDateToMidnight(inputDate, nextDay) {
+  const [year, month, date] = [
+    inputDate.getFullYear(),
+    inputDate.getMonth(),
+    inputDate.getDate()
+  ];
+
+  if (nextDay === false) {
+    return new Date(year, month, date);
+  } else if (nextDat === true) {
+    const output = new Date(year, month, date);
+    output.setDate(output.getDate() + 1);
     return output;
   }
-  
-  function getMidnight(inputDate) {
-    const [ year, month, date ] = [
-      inputDate.getFullYear(),
-      inputDate.getMonth(),
-      inputDate.getDate()
-    ]
-    return new Date(year, month, date);
-  }
-  
-  function msToHrs(t) {
-    return (( (t/1000) / 60 ) / 60).toPrecision(2)
-  }
-  
+}
+
+function yearMonthDateMatch(dateA, dateB) {
+  const [yearA, monthA, dayOfMonthA] = [
+    dateA.getFullYear(),
+    dateA.getMonth(),
+    dateA.getDate()
+  ];
+
+  const [yearB, monthB, dayOfMonthB] = [
+    dateB.getFullYear(),
+    dateB.getMonth(),
+    dateB.getDate()
+  ];
+
+  if (yearA === yearB && monthA === monthB && dayOfMonthA === dayOfMonthB)
+    return true;
+
+  return false;
+}
---
+++ todo.md
@@ -0,0 +1,19 @@
+- get by value change to query...
+  - search endpoint?
+    https://developers.google.com/apps-script/reference/spreadsheet/text-finder
+    https://developers.google.com/apps-script/reference/spreadsheet/sheet#createtextfinderfindtext
+  - get rows by multiple queries
+  - get ROWSSS
+  - max rows, max cols /// max cells.
+
+interact with json instead of arrays...?? ?? ??
+
+### Reliability
+
+- Lock
+- Caching
+- Parentless Data
+
+- maintenance
+  - sort https://developers.google.com/apps-script/reference/spreadsheet/sheet#sort(Integer)
+  - id checking
