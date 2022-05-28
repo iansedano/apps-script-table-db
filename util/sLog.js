b5925ea83deea8e93e50fcd85df12159e289896f
Ian <iansedano@gmail.com>
2022-05-28 15:34:29 +0200

    cleaning

--- util/sLog.js
+++
@@ -1,5 +0,0 @@
-function sLog(value="") {
-    const file = SpreadsheetApp.openById(SS_ID);
-    const sheet = file.getSheetByName('post_log');
-    sheet.appendRow([new Date(), JSON.stringify(value)])
-  }
